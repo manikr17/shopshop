ce94a305a41011fb07d874d15f40a4693eb5d24a
code_10 <manojkumar333619@gmail.com>
2020-04-27 21:08:36 +0530

    Update login_details.php

--- login/login_details.php
+++ login/login_details.php
@@ -1,3 +1,47 @@
 <?php
-  echo "login";
+
+  session_start();
+  //including for calling useful functions 
+  include '../libraries/chocolates.php';
+
+  //getting username and password from login.php
+  $u = $_POST['user_name'];
+  $p = $_POST['password'];
+
+  function check_password($user_name,$password)
+  {
+    
+    $con = getCon();
+    
+    $user = $con->query("select * from user where user_name='$user_name;")->fetch_assoc();
+    echo var_dump($user)."<br>";
+    
+    $password_hash=$user['password'];
+    
+    if(password_verify($p,$password_hash))
+      return True;
+    else
+      return False;
+    
+  }
+
+  //first parameter - user is table,
+  //second parameter - column name in table,
+  //third parameter - variable created by getting user_name
+  if(rowExists('user','user_name',$u)){
+    if(check_password($u,$p)){
+        $_SESSION['user_name']=$u;
+        header("Location:../index.php");
+        die();
+    }
+    else
+        echo "no2 [password wrong]";
+  }
+  else
+    echo "no1 [user doesn't exist]";
+
+  
+  
+  
+
 ?>
