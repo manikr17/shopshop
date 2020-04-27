579b19c2c05c456f51b5688ee0215624c3288e4b
code_10 <manojkumar333619@gmail.com>
2020-04-27 13:14:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -34,10 +34,10 @@
           <a href="#" class="nav-item nav-link">Register</a>
           <a href="#" class="nav-item nav-link">Login</a>
         </div>
-        <!--<form class="form-inline ml-auto">
-            <input type="text" class="form-control mr-sm-2" placeholder="Search">
-            <button type="submit" class="btn btn-outline-light">Search</button>
-        </form>-->
+        <form class="form-inline">
+            <input type="text" class="form-control mr-sm-2" placeholder="Search" aria-label="search">
+            <button type="submit" class="btn btn-light my-sm-0">Search</button>
+        </form>
     </div>
 </nav>
 <div class="jumbotron jumbotron-fluid">
