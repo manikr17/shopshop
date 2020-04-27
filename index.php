b6f5f6ca72f06f6754434c03c2ea2d01812d3684
code_10 <manojkumar333619@gmail.com>
2020-04-27 12:29:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -31,8 +31,10 @@
             <a href="#" class="nav-item nav-link active">Home</a>
             <a href="#" class="nav-item nav-link">About</a>
             <a href="#" class="nav-item nav-link">Products</a>
-            <a href="#" class="nav-item nav-link">Register</a>/
-            <a href="#" class="nav-item nav-link">Login</a>
+          <a href="#" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Register</a>
+          <a href="#" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Login</a>
+            <!--<a href="#" class="nav-item nav-link">Register</a>
+            <a href="#" class="nav-item nav-link">Login</a>-->
         </div>
         <form class="form-inline ml-auto">
             <input type="text" class="form-control mr-sm-2" placeholder="Search">
