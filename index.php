9951c97bc37553c350ae6f63f009936553ffd49a
code_10 <manojkumar333619@gmail.com>
2020-04-27 13:57:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -64,7 +64,7 @@
     <div class="card m-4">
         <img src="blank.png" class="card-img-top" alt="Product">
         <div class="card-body">
-            <h5 class="card-title">Product <span class="fa-heart-o"></span></h5>
+            <h5 class="card-title">Product&nbsp;&nbsp;<span class="fa fa-heart-o"></span></h5>
             <p class="card-text">Product <?=$i?></p>
         </div>
         <div class="text-center m-2">
