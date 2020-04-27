c3eb77e2894c9eb7cd7fed02e0938d4471761de0
code_10 <manojkumar333619@gmail.com>
2020-04-27 21:19:46 +0530

    Update login_details.php

--- login/login_details.php
+++ login/login_details.php
@@ -4,45 +4,36 @@
   //including for calling useful functions 
   include '../libraries/chocolates.php';
 
-  //getting username and password from login.php
-  $u = $_POST['user_name'];
-  $p = $_POST['password'];
+  $user_name = $_POST['user_name'];
+$password = $_POST['password'];
 
-  function check_password($user_name,$password)
-  {
+function check_password($user_name,$password)
+{
     
-    $con = getCon();
+  $con = getCon();
     
-    $user = $con->query("select * from user where user_name='$user_name;");
-    $res = $user->fetch_assoc();
-    echo var_dump($res)."<br>";
+  $user = $con->query("select * from user where user_name='$user_name';")->fetch_assoc();
+  echo var_dump($user);
+  $password_hash=$user['password'];
     
-    $password_hash=$res['password'];
-    
-    if(password_verify($p,$password_hash))
-      return True;
-    else
-      return False;
-    
-  }
+  if(password_verify($password,$password_hash))
+    return True;
+  else
+    return False;
+}
 
-  //first parameter - user is table,
-  //second parameter - column name in table,
-  //third parameter - variable created by getting user_name
-  if(rowExists('user','user_name',$u)){
-    if(check_password($u,$p)){
-        $_SESSION['user_name']=$u;
+
+if(rowExists('user','user_name',$user_name)){
+    if(check_password($user_name,$password)){
+        $_SESSION['user_name']=$user_name;
         header("Location:../index.php");
         die();
     }
     else
         echo "no2 [password wrong]";
-  }
-  else
+}
+else
     echo "no1 [user doesn't exist]";
-
-  
-  
   
 
 ?>
