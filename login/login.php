21e8019a468c3fc90421fa34240a86a75e5fe304
code_10 <manojkumar333619@gmail.com>
2020-04-27 19:33:47 +0530

    Update login.php

--- login/login.php
+++ login/login.php
@@ -52,8 +52,31 @@
     </div>
 </nav>
   
+    
+  <div class="jumbotron">
+     <div class="text-center">
+        <p class="display-4">Login</p>       
+     </div>
+  </div>
   
-  
+
+    
+    
+  <form>
+     <div class="form-group">
+        <label for="inputEmail">Username</label>
+        <input type="text" class="form-control" id="inputuser_name" placeholder="user_name">
+    </div>
+    <div class="form-group">
+        <label for="inputPassword">Password</label>
+        <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+    </div>
+    <!--<div class="form-group">
+        <label class="form-check-label"><input type="checkbox"> Remember me</label>
+    </div>-->
+    <button type="submit" class="btn btn-dark">Sign in</button>
+</form>
+    
   
   
   
