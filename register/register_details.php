56766f1a5125594fb37f17f6cc9a72379f3375c7
code_10 <manojkumar333619@gmail.com>
2020-04-27 21:01:34 +0530

    Update register_details.php

--- register/register_details.php
+++ register/register_details.php
@@ -1,14 +1,19 @@
 <?php
 
+  //including for calling useful functions 
   include '../libraries/chocolates.php';
 
+  //to get connection from db
   $con=getCon();
 
+  //getting username and password from regsiter.php
   $u = $_POST['user_name'];
   $e = $_POST['email'];
   $p = $_POST['password'];
   $p = password_hash($p,PASSWORD_DEFAULT);
 
+
+  //checking if query is valid if yes then yes else respective error will be displayed
   if(($con->query("insert into user(user_name,email,password) values('$u','$e','$p');"))===True)
     echo "YES";
   else
