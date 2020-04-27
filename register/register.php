fef7ea705c376ea7ffc7c4ac8cde8c39c78a4647
code_10 <manojkumar333619@gmail.com>
2020-04-27 19:29:34 +0530

    Update register.php

--- register/register.php
+++ register/register.php
@@ -52,7 +52,34 @@
 </nav>
   
   
+ <div class="jumbotron">
+     <div class="text-center">
+        <p class="display-4">Register</p>       
+     </div>
+  </div>
   
+  
+ <!--Regsiter form-->   
+  <form>
+     <div class="form-group">
+        <label for="inputEmail">Username</label>
+        <input type="text" class="form-control" id="inputuser_name" placeholder="user_name">
+    </div>
+    <div class="form-group">
+        <label for="inputEmail">Email</label>
+        <input type="email" class="form-control" id="inputEmail" placeholder="email">
+    </div>
+    <div class="form-group">
+        <label for="inputPassword">Password</label>
+        <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+    </div>
+    <div class="form-group">
+        <label class="form-check-label"><input type="checkbox"> Remember me</label>
+    </div>
+    <button type="submit" class="btn btn-dark">Register</button>
+</form>
+    
+    
   
   
   
