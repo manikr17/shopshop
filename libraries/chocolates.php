028b2d6f4f632d65be4be25de7e21b6040e6680c
code_10 <manojkumar333619@gmail.com>
2020-04-27 16:30:39 +0530

    added functions into chocolates
    connection function

--- libraries/chocolates.php
+++ libraries/chocolates.php
@@ -1,3 +1,20 @@
 <?php
-  echo "libraries";
+
+function getCon()
+{
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
+
+    $con = new mysqli($server, $username, $password, $db);
+
+    if($con->connect_error)
+    die($con->connect_error);
+
+    return $con;
+
+}
+
 ?>
