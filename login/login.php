3c47aec7af6ec70ec80c00365c727f6a855091b8
code_10 <manojkumar333619@gmail.com>
2020-04-28 18:39:08 +0530

    Update login.php

--- login/login.php
+++ login/login.php
@@ -36,13 +36,13 @@
 
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <div class="navbar-nav">
-            <a href="../index.php" class="nav-item nav-link active">Home</a>
+            <a href="../index.php" class="nav-item nav-link">Home</a>
             <a href="../about.php" class="nav-item nav-link">About</a>
             <a href="#" class="nav-item nav-link">Products</a>
         </div>
         <div class="navbar-nav ml-auto">
             <a href="../register/register.php" class="nav-item nav-link">Register</a>
-            <a href="#" class="nav-item nav-link">Login</a>&nbsp;&nbsp;
+            <a href="#" class="nav-item nav-link active">Login</a>&nbsp;&nbsp;
             <!--<a href="#" class="nav-item nav-link active"><i class="fa fa-user-o"></i></a>-->
         </div>
         <!--<form class="form-inline">
