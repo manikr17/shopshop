27a4de649f372d3d6ea775a82b6d0951842831e1
code_10 <manojkumar333619@gmail.com>
2020-04-28 20:47:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -125,22 +125,6 @@
      </div>
     <? } ?>
     
-    
-    
-   <p class="display-4 text-center">Categories</p>
-    <? foreach($data as $dat) { ?>
-    <div class="container" id="category">
-    <div class="card-deck">
-   <div class="card">
-     <figure class="figure">
-       <img src="black.png" class="figure-img img-fluid rounded" alt="image">
-       <figcaption class="figure-caption text-center"><a href='categories/category.php?cat_id=<?=$dat['cat_id'];?>&&cat_name=<?=$dat['cat_name'];?>' class="stretched-link">category : <?=$c." : ".$data[$c-1]?></a></figcaption>
-     </figure>
-   </div> 
-      </div> 
-     </div>
-    <? } ?>
-    
   
   
 <body>
