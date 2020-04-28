b72dba8b9fd2fdd2738050be4927f4b728224286
code_10 <manojkumar333619@gmail.com>
2020-04-28 20:13:34 +0530

    created db

--- index.php
+++ index.php
@@ -110,6 +110,7 @@
     <br>
  <!--Loop category-->
     <p class="display-4 text-center">Categories</p>
+    <? foreach($data as $dat) { ?>
     <? for($j=1;$j<=2;$j++){ ?>
     <div class="container" id="category">
     <div class="card-deck">
@@ -117,14 +118,14 @@
    <div class="card">
      <figure class="figure">
        <img src="black.png" class="figure-img img-fluid rounded" alt="image">
-       <figcaption class="figure-caption text-center"><a href='categories/category.php?cat_id=<?=$data['id'];?>&&cat_name=<?=$data['cat_name'];?>' class="stretched-link">category : <?=$c." : ".$data[$c-1]?></a></figcaption>
+       <figcaption class="figure-caption text-center"><a href='categories/category.php?cat_id=<?=$dat['cat_id'];?>&&cat_name=<?=$dat['cat_name'];?>' class="stretched-link">category : <?=$c." : ".$data[$c-1]?></a></figcaption>
      </figure>
    </div> 
   <? $c++;} ?>
       </div> 
      </div>
     <? } ?>
-    
+    <? } ?>
     
    
     
