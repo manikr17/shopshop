844b9aaa442283d77e60583c175dd05efdb79488
code_10 <manojkumar333619@gmail.com>
2020-04-28 19:59:46 +0530

    connected db with category

--- index.php
+++ index.php
@@ -94,22 +94,27 @@
     
     <?php
         // category for cards
-        $data = array("books","movies","sports","medicine","clothing","grocery","electronics","beauty");
-            
+        //$data = array("books","movies","sports","medicine","clothing","grocery","electronics","beauty");
+          $con = getCon();
+        
+          $res = $con->query("select * from categories");
+          while($ele = $res->fetch_assoc())
+              $data[]=$ele;
+           
     ?>
     
     <br>
     <br>
  <!--Loop category-->
     <p class="display-4 text-center">Categories</p>
-    <? $c=1; for($j=1;$j<=2;$j++){ ?>
+    <? for($j=1;$j<=2;$j++){ ?>
     <div class="container" id="category">
     <div class="card-deck">
     <? for($i=1;$i<=4;$i++){ ?> 
    <div class="card">
      <figure class="figure">
        <img src="black.png" class="figure-img img-fluid rounded" alt="image">
-       <figcaption class="figure-caption text-center"><a href='categories/category.php?cat_id=<?=$c;?>&&cat_name=<?=$data[$c-1];?>' class="stretched-link">category : <?=$c." : ".$data[$c-1]?></a></figcaption>
+       <figcaption class="figure-caption text-center"><a href='categories/category.php?cat_id=<?=$data['id'];?>&&cat_name=<?=$data['cat_name'];?>' class="stretched-link">category : <?=$c." : ".$data[$c-1]?></a></figcaption>
      </figure>
    </div> 
   <? $c++;} ?>
