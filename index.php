5bb3a22a7b0479c8d9be5198d11a3078049c0698
code_10 <manojkumar333619@gmail.com>
2020-04-29 13:28:38 +0530

    fixed carousel

--- index.php
+++ index.php
@@ -95,23 +95,23 @@
     
     
     <!--main card carousel-->
-    <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
+    <div id="main-card" class="carousel slide m-2" data-ride="carousel">
   <div class="carousel-inner">
-    <div class="carousel-item active" data-interval="1000">
-      <img src="../black.png" class="d-block w-100" alt="..." style="height:12rem;">
+    <div class="carousel-item active" data-interval="10000">
+      <img src="../black.png" class="d-block w-100" alt="..." style="height:20rem;">
     </div>
     <div class="carousel-item" data-interval="2000">
-      <img src="../black.png" class="d-block w-100" alt="..." style="height:12rem;">
+      <img src="../black.png" class="d-block w-100" alt="..." style="height:20rem;">
     </div>
     <div class="carousel-item">
-      <img src="../black.png" class="d-block w-100" alt="..." style="height:12rem;">
+      <img src="../black.png" class="d-block w-100" alt="..." style="height:20rem;">
     </div>
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+  <a class="carousel-control-prev" href="#main-card" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
   </a>
-  <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+  <a class="carousel-control-next" href="#main-card" role="button" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
