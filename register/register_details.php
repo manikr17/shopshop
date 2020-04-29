af71369d8e30ace71c7d5ecbbf16a0ed976cc592
code_10 <manojkumar333619@gmail.com>
2020-04-29 13:57:23 +0530

    added city state country to db

--- register/register_details.php
+++ register/register_details.php
@@ -10,11 +10,14 @@
   $u = $_POST['user_name'];
   $e = $_POST['email'];
   $p = $_POST['password'];
+  $city = $_POST['city'];
+  $state = $_POST['state'];
+  $country = $_POST['country'];
   $p = password_hash($p,PASSWORD_DEFAULT);
 
 
   //checking if query is valid if yes then yes else respective error will be displayed
-  if(($con->query("insert into user(user_name,email,password) values('$u','$e','$p');"))===True)
+  if(($con->query("insert into user(user_name,email,password,city,state,country) values('$u','$e','$p','$city','$state','$country');"))===True)
     echo "YES";
   else
     echo $con->error;
